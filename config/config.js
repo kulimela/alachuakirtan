c5403083ba01adf9bc053816cc739e9b58c5b648
Bala Bosch <moonray@users.noreply.github.com>
2023-11-18 20:07:41 -0500

    Added config file

---
+++ config/config.js
@@ -0,0 +1,12 @@
+var config = {
+	placeholders: {
+		DATE: "November 24–25, 2023",
+		DAY1: "Friday, November 24, 2023",
+		DAY2: "Saturday, November 25, 2023",
+		DAY3: "Sunday, November 26, 2022",
+		YEAR: "2023",
+	},
+	IS_LIVE: false,
+	HAS_SCHEDULE: true,
+	HAS_SUNDAY: false,
+};
--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,92 @@
-/* Theme Name: Worthy - Free Powerful Theme by HtmlCoder
- * Author:HtmlCoder
- * Author URI:http://www.htmlcoder.me
- * Version:1.0.0
- * Created:November 2014
- * License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
- * File Description: Place here your custom scripts
- */
+// Load schedule
+async function loadSchedule(day) {
+	try {
+		var response = await fetch(`config/${day}.txt`);
+		var schedule = await response.text();
+		return schedule;
+	} catch (error) {
+		console.error(`Error loading schedule for ${day}:`, error);
+		return null;
+	}
+}
+
+// Replace placeholders in the entire document
+function replacePlaceholders() {
+	walk(document.body, config);
+}
+
+// Recursively walk through all child nodes of an element
+function walk(node, config) {
+	if (node.nodeType === 1) {
+		// Element node
+		for (var i = 0; i < node.childNodes.length; i++) {
+			walk(node.childNodes[i], config);
+		}
+	} else if (node.nodeType === 3) {
+		// Text node
+		replaceText(node, config);
+	}
+}
+
+// Replace placeholders in a text node
+function replaceText(node, config) {
+	var content = node.nodeValue;
+
+	var replacedContent = content.replace(
+		/\{\{([^}]+)\}\}/g,
+		function (match, p1) {
+			return config.placeholders[p1] || match;
+		}
+	);
+
+	node.nodeValue = replacedContent;
+}
+
+// Generate a table for a given day
+async function generateTable(containerId) {
+	var tableContainer = document.getElementById(containerId);
+	var day = tableContainer.dataset.day; // Get day from data-day attribute
+
+	var table = document.createElement("table");
+	table.style.width = "100%";
+	tableContainer.appendChild(table);
+
+	var colgroup = document.createElement("colgroup");
+	var col1 = document.createElement("col");
+	col1.style.width = "16%";
+	var col2 = document.createElement("col");
+	col2.style.width = "84%";
+	colgroup.appendChild(col1);
+	colgroup.appendChild(col2);
+	table.appendChild(colgroup);
+
+	var tbody = document.createElement("tbody");
+	table.appendChild(tbody);
+
+	var schedule = await loadSchedule(day);
+  console.log('schedule', schedule);
+	if (schedule) {
+		var regex = /(\d{1,2}:\d{2}\s*(?:AM|PM)?)\s+(.*)/g;
+
+		var match;
+		while ((match = regex.exec(schedule)) !== null) {
+      console.log('match', match[1], match[2]);
+			var time = match[1];
+			var performer = match[2].trim();
+
+			var row = document.createElement("tr");
+
+			var cell1 = document.createElement("td");
+			cell1.textContent = time;
+			row.appendChild(cell1);
+
+			var cell2 = document.createElement("td");
+			cell2.textContent = performer;
+			row.appendChild(cell2);
+
+			tbody.appendChild(row);
+		}
+	} else {
+		console.error(`Schedule not found for ${day}.`);
+	}
+}
