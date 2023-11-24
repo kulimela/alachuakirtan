892ecc0b6a15f309b2f6900843bdd25d991e9dfd
Bala Bosch <moonray@users.noreply.github.com>
2023-11-24 15:07:26 -0500

    fix: render youtube links differently to allow ios to not fail on initial error

--- js/custom.js
+++ js/custom.js
@@ -118,12 +118,73 @@ async function generateTable(containerId) {
 	}
 }
 
-// Replace placeholders in the entire document
-replacePlaceholders();
+// Generate a YouTube playlist for a given container
+function generateYoutube(containerElement, url) {
+	// Get type from containerElement's data-youtube attribute
+	var type = containerElement.dataset.youtube;
 
-// Call the function to generate the table for each day
-generateTable("day1Container");
-generateTable("day2Container");
-if (config.HAS_SCHEDULE && config.HAS_SUNDAY) {
-	generateTable("day3Container");
+	// Generate the iframe
+	var iframe = document.createElement("iframe");
+	iframe.setAttribute("type", "text/html");
+	iframe.setAttribute("src", url);
+	iframe.setAttribute("frameborder", "0");
+	iframe.setAttribute("allowfullscreen", "allowfullscreen");
+
+	switch (type) {
+		case "banner":
+			iframe.setAttribute("width", "1170");
+			iframe.setAttribute("height", "568");
+			break;
+		case "inline":
+			iframe.setAttribute("width", "540");
+			iframe.setAttribute("height", "262");
+			break;
+	}
+
+	// Add the iframe to the container
+	containerElement.appendChild(iframe);
 }
+
+// Find each element with data-youtube attribute and generate a YouTube playlist
+function generateYoutubes() {
+	var youtubeElements = document.querySelectorAll("[data-youtube]");
+	for (var i = 0; i < youtubeElements.length; i++) {
+		var youtubeElement = youtubeElements[i];
+		var url = config.placeholders.YOUTUBE;
+		generateYoutube(youtubeElement, url);
+	}
+}
+
+function processConfig() {
+	// Replace placeholders in the entire document
+	replacePlaceholders();
+
+	// Call the function to generate YouTube playlists
+	generateYoutubes();
+
+	// Call the function to generate the table for each day
+	generateTable("day1Container");
+	generateTable("day2Container");
+	if (config.HAS_SCHEDULE && config.HAS_SUNDAY) {
+		generateTable("day3Container");
+	}
+}
+
+// Wait for config to be loaded
+async function waitForConfig() {
+	while (typeof config === "undefined") {
+		console.log("Waiting...");
+		await new Promise((resolve) => setTimeout(resolve, 100));
+	}
+
+	// Check for document readiness using async/await
+	while (document.readyState !== "complete") {
+		await new Promise((resolve) => requestAnimationFrame(resolve));
+	}
+
+	console.log("Processing...");
+	processConfig();
+}
+
+// Call the function to wait for config
+waitForConfig();
