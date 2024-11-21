99378303e23208106b402b51a662be9d5cb27eb5
gcordua <34252868+gcordua@users.noreply.github.com>
2024-11-21 10:06:15 -0500

    fixed the dates to include sunday

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 var config = {
 	placeholders: {
-		DATE: "November 29–30, 2024",
+		DATE: "November 29–30 and December 1, 2024",
 		DAY1: "Friday, November 29, 2024",
 		DAY2: "Saturday, November 30, 2024",
 		DAY3: "Sunday, December 1, 2024",
