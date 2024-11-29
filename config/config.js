729d6367de815d266c726d6bf43952bbf8c090c1
gcordua <34252868+gcordua@users.noreply.github.com>
2024-11-28 20:12:52 -0500

    Update config.js
    updated the youtube link for 2024

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ var config = {
 		DAY2: "Saturday, November 30, 2024",
 		DAY3: "Sunday, December 1, 2024",
 		YEAR: "2024",
-		YOUTUBE: "https://www.youtube.com/embed/videoseries?list=PLaiGaaGnFr3XkmP-_Xnn6hr9gNXA6B6ph",
+		YOUTUBE: "https://www.youtube.com/embed/videoseries?list=PLaiGaaGnFr3WL1fR50C-Pi6k2o5iZGDTM",
 	},
 	IS_LIVE: false,
 	HAS_SCHEDULE: true,
