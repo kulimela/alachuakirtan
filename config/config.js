494f7e4bdc186fb886e9de074a58ed1c382cd001
Bala Bosch <moonray@users.noreply.github.com>
2024-12-08 22:48:39 -0500

    fix: when live is true, banner doesn’t strecth the height of video and title

--- config/config.js
+++ config/config.js
@@ -1,13 +1,13 @@
 var config = {
 	placeholders: {
-		DATE: "November 29–30 and December 1, 2024",
+		DATE: "November 29-30 and December 1, 2024",
 		DAY1: "Friday, November 29, 2024",
 		DAY2: "Saturday, November 30, 2024",
 		DAY3: "Sunday, December 1, 2024",
 		YEAR: "2024",
 		YOUTUBE: "https://www.youtube.com/embed/videoseries?list=PLaiGaaGnFr3WL1fR50C-Pi6k2o5iZGDTM",
 	},
-	IS_LIVE: true,
+	IS_LIVE: false,
 	HAS_SCHEDULE: true,
 	HAS_SUNDAY: true,
 };
