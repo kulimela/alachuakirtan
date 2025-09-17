5b31965445d1c1b4a9bee1582b2b808dda69239e
gcordua <34252868+gcordua@users.noreply.github.com>
2025-09-17 11:03:15 -0400

    Update config.js
    Updated the dates for 2025

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 var config = {
 	placeholders: {
-		DATE: "November 29-30 and December 1, 2024",
-		DAY1: "Friday, November 29, 2024",
-		DAY2: "Saturday, November 30, 2024",
-		DAY3: "Sunday, December 1, 2024",
-		YEAR: "2024",
+		DATE: "November 28-30, 2025",
+		DAY1: "Friday, November 28, 2025",
+		DAY2: "Saturday, November 29, 2025",
+		DAY3: "Sunday, November 30, 2025",
+		YEAR: "2025",
 		YOUTUBE: "https://www.youtube.com/embed/videoseries?list=PLaiGaaGnFr3WL1fR50C-Pi6k2o5iZGDTM",
 	},
 	IS_LIVE: false,
